aa893e5a8d96d744573db1b7e3bc3255927ff767
DanR007 <75842483+DanR007@users.noreply.github.com>
2022-06-19 21:35:04 +0700

    Initializated Window

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,66 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "main.h"
+
+#include <iostream>
+
+void glfwWindowSizeCallback(GLFWwindow* currentWindow, int size_x, int size_y)
+{
+	window_size.x = size_x;
+	window_size.y = size_y;
+	glViewport(0, 0, window_size.x, window_size.y);
+}
+
+void glfwKeyCallback(GLFWwindow* currentWindow, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(currentWindow, GLFW_TRUE);
+	}
+}
 
 int main(int argc, char** argv)
 {
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW does not work correct or cant open idk" << std::endl;
+		return -1;
+	}
+
+	window = glfwCreateWindow(window_size.x, window_size.y, "Platformer2D", NULL, NULL);
+
+	if (!window)
+	{
+		std::cerr << "Cant create a window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+	glfwSetKeyCallback(window, glfwKeyCallback);
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL())
+	{
+		std::cerr << "GLAD does not exist or cant open" << std::endl;
+		return -1;
+	}
+
+	glClearColor(1.f, 1.f, 1.f, 0.f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
 
 	return 0;
 }
---
+++ source/main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/vec2.hpp>
+
+glm::ivec2 window_size(1280, 720);
