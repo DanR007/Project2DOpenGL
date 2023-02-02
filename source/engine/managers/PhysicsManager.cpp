eecb6733dc270bf8e64e1622a1d290f0cfd5a560
Danil <75842483+DanR007@users.noreply.github.com>
2023-02-02 15:52:10 +0700

    Finally made Physics Manager more readable

--- source/engine/managers/PhysicsManager.cpp
+++ source/engine/managers/PhysicsManager.cpp
@@ -7,63 +7,70 @@
 
 #include "../../main.h"
 
-bool PhysicsManager::CanMove(Game::Actor* checkActor, const glm::vec2& nextPosition)
+bool PhysicsManager::CanMove(Game::Actor* firstActor, const glm::vec2& delta)
 {
-	std::shared_ptr<Physics::Collider> checkCollider = checkActor->GetCollider();
-	glm::vec2& sizeActor = checkCollider->GetSize();
+	std::shared_ptr<Physics::Collider> first_collider = firstActor->GetCollider();
 
 	for (std::shared_ptr<Game::Actor> actor : _world->_all_actors)
 	{
-		std::shared_ptr<Physics::Collider> currentActorCollider = actor->GetCollider();
-		glm::vec2 posCurrentCollider = actor->GetPosition(), sizeCurrentCollider = currentActorCollider->GetSize();
-		if (IsBlocking(posCurrentCollider, nextPosition, sizeActor, sizeCurrentCollider, checkCollider, currentActorCollider))
+		std::shared_ptr<Physics::Collider> second_collider = actor->GetCollider();
+		if (IsBlocking(delta, first_collider, second_collider))
+		{
+#ifdef DEBUG
+			std::cout << std::find(_world->_all_actors.begin(), _world->_all_actors.end(), actor) - _world->_all_actors.begin() <<
+				" id blocking main character by position: " << std::endl;
+#endif
+				
 			return false;
+		}
 	}
 	return true;
 }
 
-void PhysicsManager::CheckOverlapping(std::shared_ptr<Physics::Collider> checkCollider)
+void PhysicsManager::CheckOverlapping(std::shared_ptr<Physics::Collider> first_collider)
 {
-	std::shared_ptr<Game::Actor> ownerCollider;
-	glm::vec2& sizeActor = checkCollider->GetSize(), checkPosition = checkCollider->GetPosition();
 	auto it = _world->_all_actors.begin();
 	for (; it != _world->_all_actors.end(); it++)
 	{
-		std::shared_ptr<Physics::Collider> currentActorCollider = it->get()->GetCollider();
-		glm::vec2 posCurrentCollider = currentActorCollider->GetPosition(), sizeCurrentCollider = currentActorCollider->GetSize();
-		if (IsOverlap(posCurrentCollider, checkPosition, sizeActor, sizeCurrentCollider, checkCollider, currentActorCollider))
+		std::shared_ptr<Physics::Collider> second_collider = it->get()->GetCollider();
+
+		if (IsOverlap(first_collider, second_collider))
 		{
 			//Call Delegate
-			checkCollider->Overlap(it->get());
+			first_collider->Overlap(it->get());
 			break;
 		}
 			
 	}
 }
 
-bool PhysicsManager::IsOverlap(const glm::vec2& posCurrentCollider, const glm::vec2& checkPosition, const glm::vec2& sizeActor, const glm::vec2& sizeCurrentCollider,
-	std::shared_ptr<Physics::Collider> checkCollider,
-	std::shared_ptr<Physics::Collider> currentActorCollider)
+bool PhysicsManager::IsIntersection(const glm::vec2& pos_first_collider, const glm::vec2& size_first_collider, 
+	const glm::vec2& pos_second_collider, const glm::vec2& size_second_collider)
 {
-	return ((posCurrentCollider.x <= checkPosition.x + sizeActor.x && posCurrentCollider.x >= checkPosition.x && ((posCurrentCollider.y >= checkPosition.y && posCurrentCollider.y + sizeCurrentCollider.y <= checkPosition.y + sizeActor.y)
-		|| (posCurrentCollider.y + sizeCurrentCollider.y >= checkPosition.y && posCurrentCollider.y <= checkPosition.y)
-		|| (posCurrentCollider.y + sizeCurrentCollider.y >= checkPosition.y + sizeActor.y && posCurrentCollider.y <= checkPosition.y + sizeActor.y)))
-		|| (posCurrentCollider.x + sizeCurrentCollider.x >= checkPosition.x && posCurrentCollider.x <= checkPosition.x && ((posCurrentCollider.y >= checkPosition.y && posCurrentCollider.y + sizeCurrentCollider.y <= checkPosition.y + sizeActor.y)
-			|| (posCurrentCollider.y + sizeCurrentCollider.y >= checkPosition.y && posCurrentCollider.y <= checkPosition.y)
-			|| (posCurrentCollider.y + sizeCurrentCollider.y >= checkPosition.y + sizeActor.y && posCurrentCollider.y <= checkPosition.y + sizeActor.y))))
-		&& checkCollider->GetResponseType(currentActorCollider->GetObjectType()) == EResponseType::ERT_Overlap;
+	glm::vec2 end_first_collider = pos_first_collider + size_first_collider,
+		end_second_collider = pos_second_collider + size_second_collider;
+	if (pos_first_collider.x < pos_second_collider.x && end_first_collider.x > pos_second_collider.x || pos_first_collider.x >= pos_second_collider.x && pos_first_collider.x < end_second_collider.x)
+	{
+		return pos_first_collider.y < pos_second_collider.y&& end_first_collider.y > pos_second_collider.y || pos_first_collider.y >= pos_second_collider.y && pos_first_collider.y < end_second_collider.y;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool PhysicsManager::IsBlocking(const glm::vec2& posCurrentCollider, const glm::vec2& nextPosition, const glm::vec2& sizeActor, const glm::vec2& sizeCurrentCollider,
-	std::shared_ptr<Physics::Collider> checkCollider,
-	std::shared_ptr<Physics::Collider> currentActorCollider)
+bool PhysicsManager::IsOverlap(std::shared_ptr<Physics::Collider> first_collider,
+	std::shared_ptr<Physics::Collider> second_collider)
 {
-	return ((posCurrentCollider.x <= nextPosition.x + sizeActor.x && posCurrentCollider.x >= nextPosition.x && ((posCurrentCollider.y >= nextPosition.y && posCurrentCollider.y + sizeCurrentCollider.y <= nextPosition.y + sizeActor.y)
-		|| (posCurrentCollider.y + sizeCurrentCollider.y >= nextPosition.y && posCurrentCollider.y <= nextPosition.y)
-		|| (posCurrentCollider.y + sizeCurrentCollider.y >= nextPosition.y + sizeActor.y && posCurrentCollider.y <= nextPosition.y + sizeActor.y)))
-		|| (posCurrentCollider.x + sizeCurrentCollider.x >= nextPosition.x && posCurrentCollider.x <= nextPosition.x && ((posCurrentCollider.y >= nextPosition.y && posCurrentCollider.y + sizeCurrentCollider.y <= nextPosition.y + sizeActor.y)
-			|| (posCurrentCollider.y + sizeCurrentCollider.y >= nextPosition.y && posCurrentCollider.y <= nextPosition.y)
-			|| (posCurrentCollider.y + sizeCurrentCollider.y >= nextPosition.y + sizeActor.y && posCurrentCollider.y <= nextPosition.y + sizeActor.y))))
-		&& checkCollider->GetResponseType(currentActorCollider->GetObjectType()) == EResponseType::ERT_Block;
+	return IsIntersection(first_collider->GetPosition(), first_collider->GetSize(), second_collider->GetPosition(), second_collider->GetSize())
+		&& first_collider->GetResponseType(second_collider->GetObjectType()) == EResponseType::ERT_Overlap;
+}
+
+bool PhysicsManager::IsBlocking(const glm::vec2& delta_pos,
+	std::shared_ptr<Physics::Collider> first_collider,
+	std::shared_ptr<Physics::Collider> second_collider)
+{
+	return IsIntersection(first_collider->GetPosition() + delta_pos, first_collider->GetSize(), second_collider->GetPosition(), second_collider->GetSize())
+		&& first_collider->GetResponseType(second_collider->GetObjectType()) == EResponseType::ERT_Block;
 }
 
--- source/engine/managers/PhysicsManager.h
+++ source/engine/managers/PhysicsManager.h
@@ -16,18 +16,17 @@ class PhysicsManager
 public:
 	PhysicsManager(GameManager* world) : _world(world) {}
 
-	bool CanMove(Game::Actor* checkActor, const glm::vec2& nextPosition);
-	void CheckOverlapping(std::shared_ptr<Physics::Collider> checkCollider);
+	bool CanMove(Game::Actor* checkActor, const glm::vec2& delta);
+	void CheckOverlapping(std::shared_ptr<Physics::Collider> first_collider);
 
 private:
-	bool IsOverlap(const glm::vec2& posCurrentCollider, const glm::vec2& checkPosition, 
-		const glm::vec2& sizeActor, const glm::vec2& sizeCurrentCollider,
-		std::shared_ptr<Physics::Collider> checkCollider,
-		std::shared_ptr<Physics::Collider> currentActorCollider);
-	bool IsBlocking(const glm::vec2& posCurrentCollider, const glm::vec2& checkPosition, 
-		const glm::vec2& sizeActor, const glm::vec2& sizeCurrentCollider,
-		std::shared_ptr<Physics::Collider> checkCollider,
-		std::shared_ptr<Physics::Collider> currentActorCollider);
+	bool IsIntersection(const glm::vec2& pos_first_collider, const glm::vec2& size_first_collider, const glm::vec2& pos_second_collider, const glm::vec2& size_second_collider);
+
+	bool IsOverlap(std::shared_ptr<Physics::Collider> first_collider,
+		std::shared_ptr<Physics::Collider> second_collider);
+	bool IsBlocking(const glm::vec2& delta_pos,
+		std::shared_ptr<Physics::Collider> first_collider,
+		std::shared_ptr<Physics::Collider> second_collider);
 
 	GameManager* _world;
 
