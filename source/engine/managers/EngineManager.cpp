5fba35f422b787c8629a9661049fb44510b27c44
DanR007 <75842483+DanR007@users.noreply.github.com>
2024-01-31 13:51:22 +0700

    ADD: Добавил функцию Invoke для отложенного запуска функций без аргументов

--- source/engine/managers/EngineManager.cpp
+++ source/engine/managers/EngineManager.cpp
@@ -57,8 +57,16 @@ EngineManager::~EngineManager()
 
 void EngineManager::Begin()
 {
+#ifdef DEBUG
+	std::cout << "Begin game" << std::endl;
+#endif
 	if(_game)
+	{
 		_game->BeginPlay();
+	}
+
+	Invoke((Object*)GetWorld()->GetActors()[0], &Object::Destroy, 5.f);
+	GetWorld()->GetActors()[0]->Destroy();
 }
 
 void EngineManager::Update(const float& deltaTime)
@@ -66,9 +74,24 @@ void EngineManager::Update(const float& deltaTime)
 	_tick_check_physics += deltaTime;
 
 	{
-		std::thread t1 = std::thread(&GameManager::Update, _game, std::ref(deltaTime));
+		/*std::thread t1 = std::thread(&GameManager::Update, _game, std::ref(deltaTime));
 		_render->Update(deltaTime);
 		t1.join();
+*/
+		//смотрим вышло ли время на вызов какой-то функции
+		for(auto it = _invoke_functions.begin(); it != _invoke_functions.end(); )
+		{
+			(*it).second -= deltaTime;
+			if((*it).second <= 0)
+			{
+				(*it).first.operator()();
+				it = _invoke_functions.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
 	}
 	_memory->Update();
 }
--- source/engine/managers/EngineManager.h
+++ source/engine/managers/EngineManager.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
-
+#include "../Delegate.h"
 
 class MemoryManager;
 class GameManager;
@@ -13,6 +14,9 @@ namespace Physics
 class RenderManager;
 class ResourcesManager;
 class HUDManager;
+
+class Delegate;
+class Object;
 
 class EngineManager
 {
@@ -29,11 +33,50 @@ public:
 	inline Physics::PhysicsManager* GetPhysicsManager() { return _physics; }
 	inline ResourcesManager* GetResourcesManager() { return _resources; }
 	inline HUDManager* GetHUDManager() { return _hud; }
+
+	
+	/// @brief метод для отложенного вызова метода/функции без аргументов
+	/// @tparam M 
+	/// @tparam C 
+	/// @param obj объект, который будет вызывать этот метод
+	/// @param method метод вызываемый
+	/// @param time время до вызова
+	template<class C, class M>
+	void Invoke(C* obj, M method, float time)
+	{
+		if (obj)
+		{
+			Delegate new_invoke;
+
+			_invoke_functions.push_back(std::make_pair(new_invoke, time));
+			_invoke_functions.back().first.Connect(obj, method);
+		}
+	}
+
+	/// @brief очищаем оставленные удаляемым объектом функции
+	/// @tparam C 
+	/// @param obj удаляемый объект
+	void ClearFunction(const Object* obj)
+	{
+		for(auto it = _invoke_functions.begin(); it != _invoke_functions.end(); )
+		{
+			if((*it).first.compareClass(obj))
+			{
+				it = _invoke_functions.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+	}
 private:
+	void LoadResources(char** argv);
+
+	std::vector<std::pair<Delegate, float>> _invoke_functions;
+
 	const float c_tick_before_check_physics = 0.2f;
 	float _tick_check_physics = 0.f;
-
-	void LoadResources(char** argv);
 
 	GameManager* _game = nullptr;
 	MemoryManager* _memory = nullptr;
